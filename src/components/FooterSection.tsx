996015f6224825dcb7b99a349a8cf8f79019b8a3
vidhihihihihi <gupta.74@iitj.ac.in>
2024-07-06 23:50:01 +0530

    new commit

--- src/components/FooterSection.tsx
+++ src/components/FooterSection.tsx
@@ -5,264 +5,57 @@ export type FooterSectionType = {
   className?: string;
 };
 
-const FooterSection: FunctionComponent<FooterSectionType> = ({
-  className = "",
-}) => {
+const FooterSection: FunctionComponent<FooterSectionType> = ({ className = "" }) => {
   return (
     <footer className={`footer-section ${className}`}>
-      <div className="footer" />
-      <div className="signed-by-base-breadcrumbs-el" />
-      <div className="signed-by-base-breadcrumbs-el1" />
-      <div className="signed-by-base-breadcrumbs-el2" />
-      <div className="signed-by-base-breadcrumbs-el3" />
-      <div className="signed-by-base-breadcrumbs-el4" />
-      <div className="signed-by-base-breadcrumbs-el5" />
-      <div className="signed-by-base-breadcrumbs-el6" />
-      <div className="signed-by-base-breadcrumbs-el7" />
-      <div className="signed-by-base-breadcrumbs-el8" />
-      <div className="footer-content">
-        <div className="frame-div">
-          <div className="rectangle-container">
-            <div className="frame-child2" />
-            <div className="logo1">Logo</div>
-          </div>
-          <div className="image-7-parent">
-            <img className="image-7-icon1" alt="" src="/image-7-1@2x.png" />
-            <div className="social-link">
-              <div className="social-platform">
-                 
-                <a className="a1" href="tel:+918088698016" target="_blank">
-                  <span className="span1">+918088698016</span>
-                </a>
-              </div>
+        <div className="logo-section">
+          <div className="logo">Logo</div>
+          <div className="contact-details">
+            <div className="phone">
+              <a href="tel:+918088698016">+91 8088698016</a>
+            </div>
+            <div className="email">
+              <a href="mailto:assist@marketingdna.in">assist@marketingdna.in</a>
             </div>
           </div>
-          <div className="image-8-parent">
-            <img className="image-8-icon" alt="" src="/image-8@2x.png" />
-            <div className="contact-info">
-              <div className="assistmarketingdnain">
-                 
-                <a
-                  className="assistmarketingdnain1"
-                  href="mailto:assist@eterior.in"
-                  target="_blank"
-                >
-                  <span className="assistmarketingdnain2">
-                    assist@marketingdna.in
-                  </span>
-                </a>
-              </div>
-            </div>
-          </div>
-          <div className="copyright-container-parent">
-            <div className="copyright-container">
-              <div className="copyright-border" />
-              <div className="copyright-background" />
-            </div>
-            <div className="copyright-container1">
-              <div className="copyright-container-child" />
-              <div className="copyright-container-item" />
-            </div>
+          <div className="social-icons">
+            <img src="/path-to-your-icon1.png"/>
+            <img src="/path-to-your-icon2.png"/>
+            <img src="/path-to-your-icon3.png"/>
+            <img src="/path-to-your-icon4.png"/>
           </div>
         </div>
-      </div>
-      <div className="important-links-container-parent">
-        <div className="important-links-container">
-          <div className="important-links-content">
-            <div className="important-links-header">
-              <div className="important-links">Important Links</div>
-            </div>
-            <div className="important-links-list">
-              <div className="important-link-item">
-                <div className="important-link-content">
-                  <input
-                    className="chevron-right-small"
-                    type="radio"
-                    name="radioGroup-2"
-                  />
-                </div>
-                <div className="important-link-name-parent">
-                  <div className="important-link-name">
-                    <div className="important-link-label">Link</div>
-                  </div>
-                  <img className="underline-icon" alt="" src="/underline.svg" />
-                </div>
-              </div>
-              <div className="important-link-item1">
-                <div className="chevron-right-small-wrapper">
-                  <input
-                    className="chevron-right-small1"
-                    type="radio"
-                    name="radioGroup-2"
-                  />
-                </div>
-                <div className="frame-parent1">
-                  <div className="text-wrapper">
-                    <div className="text6">Link</div>
-                  </div>
-                  <img
-                    className="underline-icon1"
-                    alt=""
-                    src="/underline.svg"
-                  />
-                </div>
-              </div>
-            </div>
-            <div className="services-link-container">
-              <div className="services-link-content">
-                <input
-                  className="chevron-right-small2"
-                  type="radio"
-                  name="radioGroup-2"
-                />
-              </div>
-              <div className="services-link-name-parent">
-                <div className="services-link-name">
-                  <div className="services-link-label">Link</div>
-                </div>
-                <img className="underline-icon2" alt="" src="/underline.svg" />
-              </div>
-            </div>
-          </div>
-          <div className="featured-services-container">
-            <div className="featured-services-content">
-              <div className="featured-services-list">
-                <div className="featured-services">Featured Services</div>
-                <div className="featured-services-items">
-                  <div className="featured-service-item">
-                    <div className="featured-service-content">
-                      <div className="chevron-right-small-container">
-                        <input
-                          className="chevron-right-small3"
-                          type="radio"
-                          name="radioGroup-1"
-                        />
-                      </div>
-                      <div className="featured-service-name-parent">
-                        <div className="featured-service-name">
-                          <div className="featured-service-label">Link</div>
-                        </div>
-                        <img
-                          className="underline-icon3"
-                          alt=""
-                          src="/underline.svg"
-                        />
-                      </div>
-                    </div>
-                    <div className="featured-service-content1">
-                      <div className="chevron-right-small-frame">
-                        <input
-                          className="chevron-right-small4"
-                          type="radio"
-                          name="radioGroup-1"
-                        />
-                      </div>
-                      <div className="frame-parent2">
-                        <div className="text-container">
-                          <div className="text7">Link</div>
-                        </div>
-                        <img
-                          className="underline-icon4"
-                          alt=""
-                          src="/underline.svg"
-                        />
-                      </div>
-                    </div>
-                  </div>
-                  <div className="newsletter-container">
-                    <div className="newsletter-content">
-                      <input
-                        className="chevron-right-small5"
-                        type="radio"
-                        name="radioGroup-1"
-                      />
-                    </div>
-                    <div className="frame-parent3">
-                      <div className="newsletter-label-wrapper">
-                        <div className="newsletter-label">Link</div>
-                      </div>
-                      <img
-                        className="underline-icon5"
-                        alt=""
-                        src="/underline.svg"
-                      />
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div className="newsletter-link-container">
-                <div className="newsletter-section">Newsletter Section</div>
-                <div className="newsletter-link-items">
-                  <div className="newsletter-link-item">
-                    <div className="newsletter-link-content">
-                      <input
-                        className="chevron-right-small6"
-                        type="radio"
-                        name="radioGroup-3"
-                      />
-                    </div>
-                    <div className="frame-parent4">
-                      <div className="newsletter-link-label-wrapper">
-                        <div className="newsletter-link-label">Link</div>
-                      </div>
-                      <img
-                        className="underline-icon6"
-                        alt=""
-                        src="/underline.svg"
-                      />
-                    </div>
-                  </div>
-                  <div className="newsletter-link-item1">
-                    <div className="chevron-right-small-wrapper1">
-                      <input
-                        className="chevron-right-small7"
-                        type="radio"
-                        name="radioGroup-3"
-                      />
-                    </div>
-                    <div className="frame-parent5">
-                      <div className="text-frame">
-                        <div className="text8">Link</div>
-                      </div>
-                      <img
-                        className="underline-icon7"
-                        alt=""
-                        src="/underline.svg"
-                      />
-                    </div>
-                  </div>
-                </div>
-                <div className="newsletter-link">
-                  <div className="container8">
-                    <input
-                      className="chevron-right-small8"
-                      type="radio"
-                      name="radioGroup-3"
-                    />
-                  </div>
-                  <div className="container-group">
-                    <div className="container9">
-                      <div className="text9">Link</div>
-                    </div>
-                    <img
-                      className="underline-icon8"
-                      alt=""
-                      src="/underline.svg"
-                    />
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="copyright">
-              <div className="rentifi-all-rights">
-                © 2023 Rentifi. All Rights Reserved.
-              </div>
-            </div>
+        <div className="important-links">
+          <h3>Important Links</h3>
+          <ul>
+            <li><a href="about-us">About us</a></li>
+            <li><a href="#">Contact Us</a></li>
+            <li><a href="#">Write for us</a></li>
+            <li><a href="#">Career</a></li>
+            <li><a href="#">FAQs</a></li>
+            <li><a href="#">Blogs</a></li>
+          </ul>
+        </div>
+        <div className="featured-services">
+          <h3>Featured Services</h3>
+          <ul>
+            <li><a href="#">Search Engines Optimisation</a></li>
+            <li><a href="#">Social Media Advertising</a></li>
+            <li><a href="#">Content Marketing</a></li>
+            <li><a href="#">Web Development</a></li>
+            <li><a href="#">UI/UX Designing</a></li>
+            <li><a href="#">Google Ads</a></li>
+          </ul>
+        </div>
+        <div className="newsletter-section">
+          <h3>Newsletter Section</h3>
+          <div className="newsletter-signup">
+            <input type="email" placeholder="Enter your email" />
+            <button>Subscribe</button>
           </div>
         </div>
-        <div className="background2">
-          <div className="footer-background" />
-        </div>
+      <div className="footer-bottom">
+        <p>© 2024 DNA Marketing. All Rights Reserved.</p>
       </div>
     </footer>
   );
--- src/components/MainHeader.tsx
+++ src/components/MainHeader.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Select, MenuItem, FormControl, SelectChangeEvent } from "@mui/material";
+import { useNavigate } from "react-router-dom";
 import "./MainHeader.css";
 
 interface DropdownMenuProps {
@@ -9,38 +10,41 @@ interface DropdownMenuProps {
   menuItems: string[];
 }
 
-const DropdownMenu: React.FC<DropdownMenuProps> = ({ defaultValue, width, iconSrc, menuItems }) => (
-  <FormControl className="dropdown-menu" style={{ width }}>
-    <Select
-      defaultValue={defaultValue}
-      disableUnderline
-      displayEmpty
-      IconComponent={() => <img width="16" height="16" src={iconSrc} alt="dropdown icon" />}
-      onChange={(event: SelectChangeEvent<string>) => {
-        // Handle change if needed
-      }}
-    >
-      {menuItems.map((item, index) => (
-        <MenuItem key={index} value={item}>
-          {item}
-        </MenuItem>
-      ))}
-    </Select>
-  </FormControl>
-);
+const DropdownMenu: React.FC<DropdownMenuProps> = ({ defaultValue, width, iconSrc, menuItems }) => {
+  const navigate = useNavigate();
+
+  const handleChange = (event: SelectChangeEvent<string>) => {
+    const selectedItem = event.target.value as string;
+    const basePath = defaultValue.toLowerCase().replace(/\s+/g, "-");
+    navigate(`/${basePath}`);
+  };
+
+  return (
+    <FormControl className="dropdown-menu" style={{ width }}>
+      <Select
+        defaultValue=""
+        disableUnderline
+        displayEmpty
+        IconComponent={() => <img width="16" height="16" src={iconSrc} alt="dropdown icon" />}
+        onChange={handleChange}
+        renderValue={(value) => (value ? value : defaultValue)}
+      >
+        {menuItems.filter(item => item !== defaultValue).map((item, index) => (
+          <MenuItem key={index} value={item}>
+            {item}
+          </MenuItem>
+        ))}
+      </Select>
+    </FormControl>
+  );
+};
 
 const MainHeader: React.FC<{ className?: string }> = ({ className = "" }) => {
-  const menuItems: DropdownMenuProps[] = [
-    { defaultValue: "Digital Marketing", width: "15%", iconSrc: "/chevron-icon.svg", menuItems: ["Digital Marketing","Paid Advertising Services","Search Engine Marketing","Social Media Marketing","Graphics Designing"] },
-    { defaultValue: "SEO Service", width: "15%", iconSrc: "/chevron-icon.svg", menuItems: ["SEO Service","On Page SEO","Off Page SEO","Technical SEO service","Local SEO Service","Content Marketing"] },
-    { defaultValue: "Social Media Marketing", width: "18%", iconSrc: "/chevron-icon.svg", menuItems: ["Social Media Marketing","Facebook Marketing","LinkedIn Marketing","Instagram Marketing","YouTube Marketing"] },
-    { defaultValue: "Analytics & Tracking", width: "18%", iconSrc: "/chevron-icon.svg", menuItems: ["Analytics & Tracking"," Conversion Rate Optimisation","Google Analytics Setup & Tracking"] },
-    { defaultValue: "Services", width: "15%", iconSrc: "/chevron-icon.svg", menuItems: ["Services"," Web Designing","UI/UX Designing","Logo Designing"] },
-  ];
+  const navigate = useNavigate();
 
   return (
     <header className={`main-header ${className}`}>
-      <div className="logo-container">
+      <div className="logo-container" onClick={() => navigate("/")}>
         <a className="logo">Logo</a>
       </div>
       <nav className="nav-menu">
@@ -55,5 +59,13 @@ const MainHeader: React.FC<{ className?: string }> = ({ className = "" }) => {
     </header>
   );
 };
+
+const menuItems: DropdownMenuProps[] = [
+  { defaultValue: "Digital Marketing", width: "15%", iconSrc: "/chevron-icon.svg", menuItems: ["All Digital Marketing Services","Paid Advertising Services","Search Engine Marketing","Social Media Marketing","Graphics Designing"] },
+  { defaultValue: "SEO Service", width: "15%", iconSrc: "/chevron-icon.svg", menuItems: ["All SEO Services","On Page SEO","Off Page SEO","Technical SEO service","Local SEO Service","Content Marketing"] },
+  { defaultValue: "Social Media Marketing", width: "18%", iconSrc: "/chevron-icon.svg", menuItems: ["Facebook Marketing","LinkedIn Marketing","Instagram Marketing","YouTube Marketing"] },
+  { defaultValue: "Analytics & Tracking", width: "18%", iconSrc: "/chevron-icon.svg", menuItems: ["All Analytics & Tracking Services","Conversion Rate Optimisation","Google Analytics Setup & Tracking"] },
+  { defaultValue: "Services", width: "15%", iconSrc: "/chevron-icon.svg", menuItems: ["Web Designing","UI/UX Designing","Logo Designing"] },
+];
 
 export default MainHeader;
